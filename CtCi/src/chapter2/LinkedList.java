97e3c72e019eaff026c79b20fcd0815ee94ac858
이준희_인터넷 <_@10.231.5.104>
2018-08-31 09:47:00 +0900

    CtCi Repository created. 2018/08/31
    First Commit!

---
+++ CtCi/src/chapter2/LinkedList.java
@@ -0,0 +1,65 @@
+package chapter2;
+
+import java.util.Random;
+
+// assume that data stored in Node is of type int - how about generic type?
+class Node {
+    int data;
+    Node next;
+
+    public Node(int n){
+        this.data = n;
+    }
+}
+
+
+public class LinkedList {
+    Node head;
+
+    public LinkedList() {
+
+    }
+
+    public LinkedList(Node node) {
+        this.head = node;
+    }
+
+    public void appendToTail(int val) {
+        Node lastNode = this.head;
+
+
+        if (lastNode == null) {
+            this.head = new Node(val);
+            return;
+        }
+
+        while (lastNode.next != null)
+            lastNode = lastNode.next;
+
+        lastNode.next = new Node(val);
+        return;
+    }
+
+    public void printList() {
+        for(Node iter=this.head; iter != null; iter=iter.next)
+            System.out.print(iter.data + " ");
+    }
+
+    public void insertNodes(int num){
+        Random rand = new Random();
+
+        for(int i=0; i<num; i++)
+            this.appendToTail(rand.nextInt(10));
+    }
+
+    public static void main(String args[]){
+        // LinkedList myList = new LinkedList(new Node(7));
+        LinkedList myList = new LinkedList();
+        myList.appendToTail(6);
+        myList.appendToTail(4);
+        myList.appendToTail(2);
+        myList.appendToTail(1);
+        myList.printList();
+
+    }
+}
---
+++ CtCi/src/chapter2/RemoveDups.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+public class RemoveDups extends LinkedList{
+
+    public void removeDuplicate() {
+
+    }
+
+    public static void main(String args[]){
+
+//        LinkedList list;
+//        list = new LinkedList();
+//        list.insertNodes(10);
+//        list.printList();
+    }
+}
