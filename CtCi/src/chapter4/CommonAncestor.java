11a4767d0287054c7dcbfd1bb184dab21e26f403
Joonhee Lee <joonhee.lee.dev@gmail.com>
2018-11-25 17:50:12 +0900

    Modified Q4.8 First Common Ancestor
    Changed implementation :
    Instead of using ArrayList, two Node instances are passed as a parameter.

--- CtCi/src/chapter4/CommonAncestor.java
+++ CtCi/src/chapter4/CommonAncestor.java
@@ -5,74 +5,53 @@ import java.util.ArrayList;
 import chapter4.BinarySearchTree.Node;
 
 public class CommonAncestor {
-	
 
-	public static Node getCommonAncestor(Node startNode, ArrayList<Node> targetNode) {
-		
-		if (startNode == null)
-			return null;
-		
-		// TODO : case when same two nodes are given as an input(exception?)
-		
-		if (countNode(startNode, targetNode) != 2) {
-			return null;
-		} else { // count == 2
-			if (countNode(startNode.left, targetNode) == 1 
-				&& countNode(startNode.right, targetNode) == 1)
-			{
-				// System.out.println("ans : " + startNode.value); // found
-				return startNode;
-			} else if ( targetNode.contains(startNode) && 
-					(countNode(startNode.left, targetNode) == 1 || countNode(startNode.right, targetNode) == 1)
-					) {
-				// System.out.println("ans : " + startNode.value); // found
-				return startNode;
-			} else {
-				Node l = getCommonAncestor(startNode.left, targetNode);
-				Node r = getCommonAncestor(startNode.right, targetNode);
+	private static Boolean covers(Node r, Node n) {
+		if (r == null || n == null)
+			return false;
 
-				return (l==null)?r:l;
-			}
-		}
+		if (r == n)
+			return true;
+		else
+			return covers(r.left, n) || covers(r.right, n);
 	}
 
-	public static int countNode(Node startNode, ArrayList<Node> targetNode) {
-		
-		if (startNode == null)
-			return 0;
-		else {
-			return countNode(startNode.left, targetNode) + 
-					countNode(startNode.right, targetNode) +
-					(targetNode.contains(startNode) ? 1 : 0);
-		}
+	/* problem : covers func repeatedly searches same node. */
+	public static Node getCommonAncestor(Node root, Node p, Node q) {
+
+		// when either of nodes are not covered by the tree starting from the root.
+		if (covers(root, p) == false || covers(root, q) == false )
+			return null;
+
+		if (root == null || root == p || root == q)
+			return root;
+
+		boolean isNodepOnLeft = covers(root.left, p);
+		boolean isNodeqOnLeft = covers(root.left, q);
+
+		// when two of nodes are on different side, root is the first common ancestor.
+		if (isNodepOnLeft != isNodeqOnLeft)
+			return root;
+
+		// search for the sub-tree having both p, q as a node.
+		Node searchRoot = isNodepOnLeft ? root.left : root.right;
+		return getCommonAncestor(searchRoot, p, q);
 	}
-		
+
 	public static void main(String[] args) {
 		BinarySearchTree bst = new BinarySearchTree();
+
 		
-		/*bst.insert(21);
-		bst.insert(32);
-		bst.insert(15);
-		bst.insert(20);
-		bst.insert(8);
-		bst.insert(19);
-		bst.insert(5);
-		bst.insert(36);
-		bst.insert(26);
-		bst.insert(13);*/
-		
-		bst.build(10, 20);
+		bst.build(20, 40);
 		System.out.println();
-		
-		ArrayList<Node> al = new ArrayList<>();
-		
-		al.add(bst.getRandomNode2(bst.getRoot()));
-		al.add(bst.getRandomNode2(bst.getRoot()));
+
+		Node a = bst.getRandomNode(bst.getRoot());
+		Node b = bst.getRandomNode(bst.getRoot());
 						
-		System.out.println("first node to find : " + al.get(0).value);
-		System.out.println("second node to find : " + al.get(1).value);
+		System.out.println("first node to find : " + a.value);
+		System.out.println("second node to find : " + b.value);
 				
-		Node a = CommonAncestor.getCommonAncestor(bst.getRoot(), al);
-		System.out.println("common ancestor : " + a.value);
+		Node r = CommonAncestor.getCommonAncestor(bst.getRoot(), a, b);
+		System.out.println("common ancestor : " + r.value);
 	}
 }
