40aa649b01ec9c8d3c6f75c64adf364a6b0598ca
이준희_인터넷 <_@10.231.5.104>
2018-12-09 21:17:04 +0900

    Modified Q5.7 Pairwise Swap
    Changed arithmetic right shift(>>) to logical right shift(>>>),
    So that sign bit is filled with zero.

--- CtCi/src/chapter5/PairwiseSwap.java
+++ CtCi/src/chapter5/PairwiseSwap.java
@@ -3,11 +3,15 @@ package chapter5;
 public class PairwiseSwap {
 	public static int swap(int n) {
 	
-		return ((n & 0xAAAAAAAA) >> 1) | ((n & 0x55555555) << 1);
+		// >>> : zero-extension, >> : sign-extension
+		// <<< : doesn't exist. Only << when shifting left
+		return ((n & 0xAAAAAAAA) >>> 1) | ((n & 0x55555555) << 1);
 	}
 	
 	public static void main(String[] args) {
-		int n = 948372637;
+		// int n = Integer.MIN_VALUE;
+		
+		int n = -24349352;
 		
 		System.out.println(Integer.toBinaryString(n));
 		System.out.println(Integer.toBinaryString(swap(n)));
